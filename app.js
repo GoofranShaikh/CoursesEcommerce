ce39916a44c22717d664138bb5513bb3bd9a659c
Goofran Shaikh <ext02@dispritv.com>
2024-01-02 11:08:40 +0530

    added cors middleware

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 var bodyParser = require('body-parser');
 const port = process.env.PORT ||5000;
 const adminRoute = require('./routes/AdminRoutes');
 const courseRoute = require('./routes/CoursesRoutes')
-
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/admin',adminRoute)
